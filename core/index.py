f34cf33f5c57533af9d3c09703358bd020111ab1
Jussi Heinonen <heinonen.jussi@gmail.com>
2021-03-17 16:32:02 +0000

    Integrate IPTCInfo module with newly designed index.py

--- core/index.py
+++ core/index.py
@@ -1,10 +1,13 @@
 import json, sys, os
 import urllib.parse
 import boto3
+from iptcinfo3 import IPTCInfo
+from functions import writeDictionaryToDDB
 
 print('Loading function')
 
 IS_OFFLINE = os.environ.get('IS_OFFLINE')
+INDEX_TABLE = os.environ['INDEX_TABLE']
 
 if IS_OFFLINE:
     s3_client = boto3.client(
@@ -14,8 +17,18 @@ if IS_OFFLINE:
         aws_access_key_id = 'test',
         aws_secret_access_key = 'test'
     )
+
+    ddb_client = boto3.client(
+        'dynamodb',
+        region_name = 'localhost',
+        endpoint_url = 'http://localhost:4566',
+        aws_access_key_id = 'whatever',
+        aws_secret_access_key = 'whatever'
+    )
+
 else:
     s3_client = boto3.client('s3')
+    ddb_client = boto3.client('dynamodb')
 
 
 def lambda_handler(event, context):
@@ -31,7 +44,45 @@ def lambda_handler(event, context):
         print("CONTENT TYPE: " + response['ContentType'])
         response_body = response['Body'].read() # StreamingBody to bytes
         print('Response Body size ' + str(len(response_body)) + ' bytes')
-        return response['ContentType']
+        #return response['ContentType']
+
+        with open("tmp.jpg", "wb") as binary_file: #Write bytes to file so that IPTCInfo class can access it
+            binary_file.write(response_body)
+
+        info = IPTCInfo("tmp.jpg", force=True, inp_charset='UTF-8') # creates iptcinfo3.IPTCInfo object
+        fields = [
+            'keywords',
+            'headline',
+            'by-line',
+            'by-line title',
+            'caption/abstract',
+            'source',
+            'copyright notice',
+            'special instructions',
+            'city',
+            'country/primary location code',
+            'country/primary location name',
+            'writer/editor',
+            'object name',
+            'date created',
+            'time created',
+
+            ]
+        dict_info = {}
+        for field in fields:
+            try:
+                if info[field]:
+                    dict_info[field] = info[field]
+            except KeyError:
+                print('Failed to find key ' + field)
+
+        if len(dict_info) == 0:
+            print('No IPTC data found')
+        else:
+            # About DDB schema https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/SampleData.html
+            print('Size fo the object dict_info: ' + str(sys.getsizeof(dict_info)) + " bytes")
+            print('Dictionary object dict_info has ' + str(len(dict_info)) + " items")
+            writeDictionaryToDDB(dict_info, INDEX_TABLE, ddb_client)
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
