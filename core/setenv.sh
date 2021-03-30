8ae1b68f77da40f700d924eeb6d6b13467060f26
Jussi Heinonen <heinonen.jussi@gmail.com>
2021-03-30 12:14:51 +0100

    Adding check whether sript is sourced or subshelled

--- core/setenv.sh
+++ core/setenv.sh
@@ -8,17 +8,30 @@ declare -A SETTINGS TMP_SETTINGS
 
 export DIRNAME="$(dirname $0)"
 
+usage() {
+    echo "USAGE: source $0"
+}
+
+
 echo "DIRNAME is ${DIRNAME}"
 
+
+
+if [[ "$$" -ne "1" ]]; then
+    echo "Source this script instead of running in subshell"
+    usage
+    exit 1
+fi
+
 SETTINGS[INDEX_TABLE]="ltd.noooner.dam.core.index" 
-SETTINGS[UPLOAD_BUCKET]="ltd.noooner.dam.core.uploads"
-SETTINGS[ORIGINALS_BUCKET]="ltd.noooner.dam.core.originals"
+SETTINGS[UPLOAD_BUCKET]="ltd.noooner.dam.core.upload"
+SETTINGS[DOWNLOAD_BUCKET]="ltd.noooner.dam.core.download"
 SETTINGS[IS_OFFLINE]="true"
 
 
 read -p "DynamoDB table name for Index data? [default: ${SETTINGS[INDEX_TABLE]}] " TMP_SETTINGS[INDEX_TABLE]
-read -p "S3 bucket name for UPLOADS? [default: ${SETTINGS[UPLOAD_BUCKET]}] " TMP_SETTINGS[UPLOAD_BUCKET]
-read -p "S3 bucket name for ORIGINALS? [default: ${SETTINGS[ORIGINALS_BUCKET]}] " TMP_SETTINGS[ORIGINALS_BUCKET]
+read -p "S3 bucket name for UPLOAD? [default: ${SETTINGS[UPLOAD_BUCKET]}] " TMP_SETTINGS[UPLOAD_BUCKET]
+read -p "S3 bucket name for DOWLOAD? [default: ${SETTINGS[DOWNLOAD_BUCKET]}] " TMP_SETTINGS[DOWNLOAD_BUCKET]
 read -p "Using LocalStack for backend services? [default: ${SETTINGS[IS_OFFLINE]}] " TMP_SETTINGS[IS_OFFLINE]
 
 for each in ${!SETTINGS[*]}; do
@@ -32,13 +45,13 @@ done
 
 export INDEX_TABLE=${SETTINGS[INDEX_TABLE]}
 export UPLOAD_BUCKET=${SETTINGS[UPLOAD_BUCKET]}
-export ORIGINALS_BUCKET=${SETTINGS[ORIGINALS_BUCKET]}
+export ORIGINALS_BUCKET=${SETTINGS[DOWNLOAD_BUCKET]}
 export IS_OFFLINE=${SETTINGS[IS_OFFLINE]}
 
 read -p "Create LocalStack resources? [y/n]" localstack
 
 if [[ "${localstack}" == "y" ]]; then
-    source $(find . -name localstack-resources.sh)
+    source $(find ../ -name localstack-resources.sh)
 else
     echo "Skipping LocalStack resource creations"
 fi
