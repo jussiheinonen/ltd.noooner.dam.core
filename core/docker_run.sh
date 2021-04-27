e896866c71c7f8ca6c3734a1bdfc1965cf797404
Jussi Heinonen <heinonen.jussi@gmail.com>
2021-04-27 11:25:32 +0100

    Add docker_run.sh, front end of docker runtimes

---
+++ core/docker_run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Front end for managing Docker runtimes
+
+declare -A OPTIONS_CMD OPTIONS_DESC
+
+OPTIONS_CMD[1]="sudo docker run --rm -p 4566:4566 -p 4571:4571 localstack/localstack"
+OPTIONS_CMD[2]="sudo docker run --rm --entrypoint /bin/bash -v $(pwd):/usr/app/ --net=host -it wiren:alpine"
+OPTIONS_CMD[3]="sudo docker run --rm --entrypoint /bin/bash -v $(pwd):/usr/app/ -v /home/jussi/.aws/:/root/.aws/ --net=host -it wiren:alpine"
+
+OPTIONS_DESC[1]="Localstack backend"
+OPTIONS_DESC[2]="Wiren development runtime for local development (local .aws/credentials)"
+OPTIONS_DESC[3]="Wiren runtime for AWS API (real .aws/credentials)"
+
+banner_please() {
+    echo  " "
+    echo "START DOCKER CONTAINERS"
+    echo  " "
+    echo "Select one of the following options"
+    echo  " "
+}
+
+banner_please
+
+for each in ${!OPTIONS_DESC[*]}; do
+    echo -e "\t${each}. ${OPTIONS_DESC[${each}]}"
+done
+
+echo  " "
+read -p "Which container to start? " INDEX
+
+if [[ "${INDEX}" -gt "${#OPTIONS_CMD[*]}" ]]; then 
+    echo "No container number ${#OPTIONS_CMD[*]}. Adios amigo."
+    exit 1
+else
+    echo "Starting container ${OPTIONS_DESC[${INDEX}]}"
+    ${OPTIONS_CMD[${INDEX}]}
+fi
