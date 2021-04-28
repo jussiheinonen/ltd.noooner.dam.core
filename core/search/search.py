e52154fabf3df94b448ced1fb181abd4d8b61c12
Jussi Heinonen <heinonen.jussi@gmail.com>
2021-04-28 15:04:46 +0100

    Add multiple keyword support for Search API

--- core/search/search.py
+++ core/search/search.py
@@ -1,6 +1,6 @@
 from functions import *
 from pprint import pprint
-import boto3, json, os, time, requests
+import boto3, json, os, time
 
 IS_OFFLINE = os.environ.get('IS_OFFLINE')
 INDEX_TABLE = os.environ.get('INDEX_TABLE')
@@ -29,7 +29,8 @@ def ddb_scan_a_word_in_field(client, table, word, field):
     word: a keyword to search for in field, eg. mardrid
     field: Field in DynamoDB Item to search against
     '''
-    projection_ex='id, n_search, headline,original_filename, upload_time'
+    
+    projection_ex='id, headline,original_filename, upload_time'
     filter_ex='contains(' + field + ',:key)'
     expression_at_val={
                     ":key":{
@@ -45,68 +46,124 @@ def ddb_scan_a_word_in_field(client, table, word, field):
         )
     return response
 
-def getDownloadUrl(filename):
-    url = PRESIGN_ENDPOINT_URL
-    payload = {
-        "method": "get_object",
-        "filename": filename
+def get_metadata(item):
+    '''
+    example_input = {
+        'upload_time': {
+            'N': '1619604041'
+            }, '
+        original_filename': {
+            'S': '2020-12-07T142510Z_33248525_RC2EIK97VAWP_RTRMADP_3_SOCCER-AFCCHAMPIONS-YOK-SUW.JPG'
+            }, 
+        'id': {
+            'S': '6dafa7dc8b7825827f20ec10fbab2213'
+            }, 
+        'headline': {
+            'S': 'AFC Champions League - Round of 16 - Yokohama F Marinos v Suwon Bluewings'
+            }
+        }
+
+    example_output = {
+        "get_download_url": get_download_url,
+        "headline": headline,
+        "upload_time": upload_time
     }
-    headers = {
-        "Content-Type": "application/json"
-    }
+    '''
+    metadata_dict = {}
 
     try:
-        response = requests.post(url, data = json.dumps(payload), headers=headers)
-        return response.text
+        headline = item['headline']['S']
     except:
-        return False
+        headline = "N/A"
+    org_file = item['original_filename']['S']
+    epoch_upload_time = int(item['upload_time']['N'])
+    upload_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_upload_time))
+    if PRESIGN_ENDPOINT_URL:
+        get_download_url = PRESIGN_ENDPOINT_URL + '?filename=' + org_file + '&action=get_object'
+    else:
+        get_download_url = '///UNKONWN_PRESIGN_ENDPOINT_URL' + '?filename=' + org_file + '&action=get_object'
+    metadata_dict = {
+        "get_download_url": get_download_url,
+        "headline": headline,
+        "upload_time": upload_time
+    }
+
+    return(metadata_dict)
+
+def find_ids_per_keyword(results):
+    '''
+    Extract ids from results and appends them in to dictionary using id as key and list of keywords as value
+    For each item under Items key this function calls get_metadata function that extracts remaining metadata
+    At the end 2 dictionaries are merged into one and returned
+
+    example_input = {
+        'champions': {
+            'Count': 12,
+            'Items': [
+                {
+                    'headline': {
+                        'S': 'AFC Champions League - Round of '
+                                            '16 - Yokohama F Marinos v Suwon '
+                                            'Bluewings'
+                                            },
+                        'id': {
+                            'S': '2cbacbd98d286e1ca44299fe93de5c67'
+                            },
+                        'original_filename': {
+                            'S': '2020-12-07T142534Z_188824836_RC2EIK9EXWOU_RTRMADP_3_SOCCER-AFCCHAMPIONS-YOK-SUW.JPG'
+                            },
+                        'upload_time': {
+                            'N': '1619604044'
+                            }
+                        },
+
+    example_output = {
+        '00311825c1adeb60ffff79d7e8a6ee88': {
+            'get_download_url': 'https://tm7do0vu9j.execute-api.eu-west-1.amazonaws.com/presign?filename=shutterstock_editorial_Shakhtar_Donetsk_vs_Real_Madrid_11088586O.jpg&action=get_object',
+            'headline': 'Shakhtar Donetsk vs Real '
+                        'Madrid, Kiev, Ukraine - 01 '
+                        'Dec 2020',
+            'matching_keywords': ['real',
+                                  'madrid',
+                                  'champions'],
+            'relevance': 3,
+            'upload_time': '2021-04-28 10:01:02'
+            }
+        
+    '''
+    id_to_keyword = {} # Dictionary using id as a key
+    id_to_metadata = {}
+    
+    for key, value in results.items():
+        for item in value['Items']:
+            id = item['id']['S']
+            id_to_keyword.setdefault(id, []).append(key) # Solution provided by Paul Panzer in https://stackoverflow.com/questions/43060655/update-values-of-a-list-of-dictionaries-in-python
+            id_to_metadata[id] = get_metadata(item)
+    
+    # Merge id_to_metadata and id_to_keyword
+    for id, keywords in id_to_keyword.items():
+        id_to_metadata[id].update(matching_keywords=keywords, relevance=len(id_to_keyword[id]))
+
+    return id_to_metadata
+
 
 
 def lambda_handler(event, context):
     params = event['queryStringParameters']['q']
     print(f"Query Parameters {params}")
-    lst_params = []
-    lst_params = params.split('+')
+    lst_params = params.split(' ') # Split at whitespace, not + sign. API GW appears to substitute + with whitespace
     print('Number of params ' + str(len(lst_params)))
 
-    print(f"Scanning table {INDEX_TABLE}")
-    if lst_params[0] == 'all': # query all with q=all
-        response = ddb_scan_all(ddb_client, INDEX_TABLE)
-    else:
-        response = ddb_scan_a_word_in_field(ddb_client, INDEX_TABLE, lst_params[0].lower(), 'n_search')
-
-    i = 0
-    results_lst = [] # list of dictionary items
-    results_json = {}
-    for item in response['Items']:
-        #pprint(item)
-        try:
-            headline = item['headline']
-        except:
-            headline = {
-                "S": "N/A"
-            }
-        org_file = item['original_filename']
-        epoch_upload_time = item['upload_time']
-        epoch_upload_time = int(epoch_upload_time['N'])
-        upload_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_upload_time))
-
-        if PRESIGN_ENDPOINT_URL:
-            #download_url = getDownloadUrl(org_file['S'])
-            get_download_url = PRESIGN_ENDPOINT_URL + '?filename=' + org_file['S'] + '&action=get_object'
-            dict_response = {
-                "get_download_url": get_download_url,
-                "headline": headline['S'],
-                "upload_time": upload_time
-
-            }
-            results_json[i] = dict_response
-            #pprint(results_json)
-
-        else:
-            print('Image' + str(i) + ': ' + org_file['S'] + ', with headline: ' + headline['S'] + ', uploaded at: ' + upload_time)
-        i += 1
-
+    response={} 
     
-    print('NUMBER OF SEARCH RESULTS: ' + str(len(response['Items'])))
+    for word in lst_params:
+        print(f"Scanning table {INDEX_TABLE} for keyword {word}")
+        if lst_params[0] == 'all': # query all with q=all
+            response['all'] = ddb_scan_all(ddb_client, INDEX_TABLE)
+        else:
+            response[word] = ddb_scan_a_word_in_field(ddb_client, INDEX_TABLE, word.lower(), 'n_search')
+    
+    results_json = find_ids_per_keyword(response)
+       
     return results_json
+
