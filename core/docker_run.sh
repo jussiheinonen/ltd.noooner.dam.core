ea2a0a769b0a07d6892d57ecf6e9ca2145859eb0
Jussi Heinonen <heinonen.jussi@gmail.com>
2021-05-21 16:04:22 +0100

    Add option 4 for Lambda-like dev environment

--- core/docker_run.sh
+++ core/docker_run.sh
@@ -7,10 +7,12 @@ declare -A OPTIONS_CMD OPTIONS_DESC
 OPTIONS_CMD[1]="sudo docker run --rm -p 4566:4566 -p 4571:4571 localstack/localstack"
 OPTIONS_CMD[2]="sudo docker run --rm --entrypoint /bin/bash -v $(pwd):/usr/app/ --net=host -it wiren:alpine"
 OPTIONS_CMD[3]="sudo docker run --rm --entrypoint /bin/bash -v $(pwd):/usr/app/ -v /home/jussi/.aws/:/root/.aws/ --net=host -it wiren:alpine"
+OPTIONS_CMD[4]="sudo docker run --rm --entrypoint /bin/bash -v $(pwd):/usr/app/ --net=host -it thumbnailer:local"
 
 OPTIONS_DESC[1]="Localstack backend"
 OPTIONS_DESC[2]="Wiren development runtime for local development (local .aws/credentials)"
 OPTIONS_DESC[3]="Wiren runtime for AWS API (real .aws/credentials)"
+OPTIONS_DESC[4]="Lambda-like dev environment (FROM public.ecr.aws/lambda/python:3.8)"
 
 banner_please() {
     echo  " "
